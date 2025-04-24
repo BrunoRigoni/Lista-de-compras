1430de83b6f6e29081258472abd8cfe536c18790
Bruno Rigoni PC <brigoni2011@gmail.com>
2025-04-24 14:13:46 -0300

    criando função que adiciona um novo item a lista.

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,65 @@ const input = document.getElementById("add-item");
 const button = document.getElementById("submitForm");
 
 //manipulando os dados do input para receber apenas strings.
-input.addEventListener("input", (e) => {
-  console.log(input.value);
+input.addEventListener("input", () => {
   let regex = /\d+$/;
-
   input.value = input.value.replace(regex, "");
 });
 
 //criando evento para garantir que a página não atualize ao enviar o formulário.
 form.onsubmit = (event) => {
   event.preventDefault();
+
 };
+
+
+
+
+function createNewItem() {
+
+//SELECIONANDO A UL
+const items = document.querySelector("ul");
+
+//CRIANDO ELEMENTOS
+const newItemAdd = document.createElement("li");
+const newChecked = document.createElement("input");
+const newLabel = document.createElement("label");
+const newCheckbox = document.createElement("span");
+const newItemName = document.createElement("p");
+const btn = document.createElement("button");
+const binImg = document.createElement("img");
+
+binImg.src = "./assets/icons/bin.svg";
+newItemAdd.classList.add("item-style");
+newItemName.textContent = "Bolo";
+
+
+newChecked.type = "checkbox";
+newLabel.classList.add("name-item");
+
+newCheckbox.classList.add("checkbox-icon");
+btn.classList.add("remove");
+
+
+newLabel.append(newCheckbox, newItemName);
+newItemAdd.append(newChecked, newLabel, btn)
+btn.append(binImg)
+
+console.log(newItemAdd);
+
+
+items.append(newItemAdd);
+}
+
+
+button.onclick = (event) => {
+  createNewItem()
+}
+
+
+
+button.onsubmit = (event) => {
+    event.preventDefault();
+
+  createNewItem()
+}
